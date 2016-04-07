1575b1e86347dd166f184c90761324172855c3ba
Engin Isik <iskengin@gmail.com>
2016-04-07 19:30:08 +0300

    fix conflict

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import json
 
-from flask import Flask
-
+from flask import Flask,jsonify,abort,make_response,request
 app = Flask(__name__)
+
+users = [
+	{
+		'user_id' : 101,
+		'username' : 'Engin',
+		'surname' : 'Isik',
+		'email' : 'iskengin@gmail.com',
+		'password' : '12345'
+	},
+	{
+		'user_id' : 102,
+		'username' : 'Mustafa',
+		'surname' : 'Hasturk',
+		'email' : 'mustafa.hasturk@yandex.com',
+		'password' : 'cokgizli'
+	}
+]
 
 @app.route('/')
 def hello():
@@ -19,14 +35,33 @@ def rootWelcome():
     name = "Mustafa"
     surname = "hasturk"
 
-    parameters = {
-    "name": name,
-    "surname": surname
+@app.route('/api/v1/memberLogin1')
+def memberLogin1():
+    memberDict = {
+        "email": "mustafa.hasturk@yandex.com",
+        "password": "cokgizli"
     }
     
     appJson = json.dumps(parameters)
     return appJson
 
+@app.route('/api/v1/memberLogin', methods=['POST'])
+def memberLogin():
+    if not request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = [user for user in users if user['email'] == request.json['email'] and user['password'] == request.json['password']]
+    if len(user) == 0:
+        return jsonify({'ServiceCode':'1', 'ExceptionMessage':'Record not found'})
+    return jsonify({'ServiceCode':'0','User': user[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}),404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad_Request'}),400)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
