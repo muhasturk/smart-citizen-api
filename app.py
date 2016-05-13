23ef66213f04b18b6d3ae236f1963b51cafa4f3c
Engin Isik <iskengin@gmail.com>
2016-05-13 04:10:01 +0300

    fix mysql password

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mustafaengin  '
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mustafaengin'
 app.config['MYSQL_DATABASE_DB'] = 'smart'
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 app.config['MYSQL_DATABASE_CHARSET'] = 'utf8'
