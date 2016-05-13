594875ba3869516916952dfa43ac0f0cef399987
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2016-05-13 06:16:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,13 @@ from flask import Flask,jsonify,abort,make_response,request
 from flaskext.mysql import MySQL
 app = Flask(__name__)
 
+
+conn = mysql.connect()
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM User ")
+r = [dict((cursor.description[i][0], value) for i, value in enumerate(row)) for row in cursor.fetchall()]
+cursor.connection.close()
+
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'burada-sıfre-var'
