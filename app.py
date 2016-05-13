9d68a69ab0628d9f3d239b38812a07c56e1d9ebd
Mustafa Hastürk <mustafa.hasturk@yandex.com>
2016-05-13 06:15:05 +0300

    t

--- app.py
+++ app.py
@@ -361,15 +361,18 @@ def acceptReport():
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(jsonify({'serviceCode': 1, 'data': None, 'exception':{'exceptionCode':400, 'exceptionMessage': 'Bad request'}}),400)
+    return make_response(jsonify({'serviceCode': 1, 'data': None, 'exception':{'exceptionCode':400, \
+    'exceptionMessage': 'Bad request'}}),400)
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'serviceCode': 1, 'data': None, 'exception':{'exceptionCode':404, 'exceptionMessage': 'Not found'}}),404)
+    return make_response(jsonify({'serviceCode': 1, 'data': None, 'exception':{'exceptionCode':404, \
+    'exceptionMessage': 'Not found'}}),404)
 
 @app.errorhandler(405)
 def not_found(error):
-    return make_response(jsonify({'serviceCode': 1, 'data': None, 'exception':{'exceptionCode':405, 'exceptionMessage': 'Method not allowed'}}),405)
+    return make_response(jsonify({'serviceCode': 1, 'data': None, 'exception':{'exceptionCode':405, \
+    'exceptionMessage': 'Method not allowed'}}),405)
 
 
 if __name__ == '__main__':
